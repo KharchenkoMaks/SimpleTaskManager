c01c4e2954e3cca22189cb0fe104497d859a595d
Maksym Kharchenko <maksym.kharchenko@m-kharchenko.teamdev.local>
2021-11-08 17:31:32 +0200

    tested Edit and Delete methods of TaskManager in main()

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,11 @@ void PrintTaskVector(const std::vector<std::pair<int, Task>>& vec){
 
 int main() {
     TaskManager task_manager;
-    task_manager.Create(Task::Create("NewTask1", Task::Priority::HIGH, time(0)));
-    task_manager.Create(Task::Create("NewTask2", Task::Priority::LOW, time(0)));
+    int id1 = task_manager.Create(Task::Create("NewTask1", Task::Priority::HIGH, time(0)));
+    int id2 = task_manager.Create(Task::Create("NewTask2", Task::Priority::LOW, time(0)));
+    int id3 = task_manager.Create(Task::Create("NewTask3", Task::Priority::NONE, time(0)));
+    task_manager.Edit(id2, Task::Create("EditedTask", Task::Priority::HIGH, time(0)));
+    task_manager.Delete(id1);
     PrintTaskVector(task_manager.Show());
     return 0;
 }
