027af1433d5579839837751a77df40e64b7461b8
Maksym Kharchenko <maksym.kharchenko@m-kharchenko.teamdev.local>
2021-11-08 16:30:15 +0200

    adding two tasks and printing it

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+#include "model/Task.h"
+#include "model/TaskManager.h"
+
+#include <vector>
+#include <utility>
+#include <ctime>
+
+void PrintTaskVector(const std::vector<std::pair<int, Task>>& vec){
+    for (std::pair<int, Task> item : vec){
+        std::cout << "ID: " << item.first << ", " << item.second.to_string();
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TaskManager task_manager;
+    task_manager.Create(Task::Create("NewTask1", Task::Priority::HIGH, time(0)));
+    task_manager.Create(Task::Create("NewTask2", Task::Priority::LOW, time(0)));
+    PrintTaskVector(task_manager.Show());
     return 0;
 }
