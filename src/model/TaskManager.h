b2f0e1d5c1fe916dd93f18acfd39d172f3b6402a
Maksym Kharchenko <maksym.kharchenko@m-kharchenko.teamdev.local>
2021-11-08 14:39:46 +0200

    added TaskManager class

--- src/model/TaskManager.h
+++ src/model/TaskManager.h
@@ -5,9 +5,18 @@
 #ifndef SIMPLETASKMANAGER_TASKMANAGER_H
 #define SIMPLETASKMANAGER_TASKMANAGER_H
 
+#include "Task.h"
+
+#include <map>
 
 class TaskManager {
-
+public:
+    int Create(Task t);
+    void Edit(Task t);
+    void Delete(Task t);
+    std::vector<std::pair<int, Task>> Show();
+private:
+    std::map<int, Task> tasks_;
 };
 
 
