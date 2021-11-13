1e720f75ea2c852d0cc0795e700cc4a77b886f92
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-13 14:45:44 +0200

    added test suite for IdGenerator

---
+++ tests/model/IdGeneratorTest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Maksym Kharchenko on 13.11.2021.
+//
+
+#include "gtest/gtest.h"
+#include "../../src/model/IdGenerator.h"
+
+class IdGeneratorTest : public ::testing::Test{
+
+};
