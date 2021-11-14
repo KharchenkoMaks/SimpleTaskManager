4b43e0120a4aa562c7191523edf492e3a6757553
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-14 18:10:14 +0200

    TaskManagerTest set up

---
+++ tests/model/TaskManagerTest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Maksym Kharchenko on 14.11.2021.
+//
+
+#include "gtest/gtest.h"
+#include "../../src/model/TaskManager.h"
+
+class TaskManagerTest : public ::testing::Test{
+    
+};
