6c0fc8b23f14b8e928877288e7ed4bb3395ac97a
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-17 16:35:13 +0200

    fixed giving instance of IdGenerator when creating TaskManager in main()

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void PrintTaskVector(const std::vector<std::pair<TaskId, Task>>& vec){
 }
 
 int main() {
-    TaskManager task_manager;
+    TaskManager task_manager(std::make_unique<IdGenerator>());
     TaskId id1 = task_manager.Create(Task::Create("NewTask1", Task::Priority::HIGH, time(0)));
     TaskId id2 = task_manager.Create(Task::Create("NewTask2", Task::Priority::LOW, time(0)));
     TaskId id3 = task_manager.Create(Task::Create("NewTask3", Task::Priority::NONE, time(0)));
