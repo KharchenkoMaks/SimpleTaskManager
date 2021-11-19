3704d54085c4d0cddca7c1586e4a5bdba41d374e
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-19 17:24:21 +0200

    added tests for operator== and completed parameter of Task class

--- tests/model/TaskTest.cpp
+++ tests/model/TaskTest.cpp
@@ -8,9 +8,36 @@
 #include <ctime>
 #include <vector>
 
-class TaskTest : public ::testing::Test{
+class TaskTest : public ::testing::Test {
 
 };
+
+TEST_F(TaskTest, NewTaskParametersShouldBeCorrect){
+    // Arrange
+    const std::string expected_title = "title";
+    const Task::Priority expected_priority = Task::Priority::MEDIUM;
+    const time_t expected_time = time(0);
+    const bool expected_completion = true;
+    const std::string expected_label = "some label";
+    // Act
+    Task task = Task::Create(expected_title,
+                             expected_priority,
+                             expected_time,
+                             expected_completion,
+                             expected_label);
+
+    const std::string actual_title = task.GetTitle();
+    const Task::Priority actual_priority = task.GetPriority();
+    const time_t actual_time = task.GetDueTime();
+    const bool actual_completion = task.IsCompleted();
+    const std::string actual_label = task.GetLabel();
+    // Assert
+    EXPECT_EQ(expected_title, actual_title);
+    EXPECT_EQ(expected_priority, actual_priority);
+    EXPECT_EQ(expected_time, actual_time);
+    EXPECT_EQ(expected_completion, actual_completion);
+    EXPECT_EQ(expected_label, actual_label);
+}
 
 // Creating Task object with title "title"
 // Task::GetTitle() should return "title"
@@ -74,18 +101,6 @@ TEST_F(TaskTest, usingToStringTaskMethod_shouldReturnCorrectString){
     }
 }
 
-// Creating task and completing it
-// Task should be completed
-TEST_F(TaskTest, shouldCompleteUncompletedTask){
-    // Arrange
-    Task task = Task::Create("title", Task::Priority::HIGH, time(0));
-    // Act
-    task.Complete();
-    bool actual = task.IsCompleted();
-    // Assert
-    EXPECT_TRUE(actual);
-}
-
 // Creating new task
 // It should be uncompleted
 TEST_F(TaskTest, newTaskShouldBeUncompleted){
@@ -93,16 +108,6 @@ TEST_F(TaskTest, newTaskShouldBeUncompleted){
     Task task = Task::Create("title", Task::Priority::HIGH, time(0));
     // Assert
     EXPECT_FALSE(task.IsCompleted());
-}
-
-// Creating new task and completing it twice
-// Should throw std::runtime_error
-TEST_F(TaskTest, TryToCompleteCompletedTask_ShouldThrowRuntimeError){
-    // Arrange
-    Task task = Task::Create("title", Task::Priority::HIGH, time(0));
-    task.Complete();
-    // Act & Assert
-    EXPECT_THROW(task.Complete(), std::runtime_error);
 }
 
 TEST_F(TaskTest, shouldReturnRightTaskLabel){
@@ -113,11 +118,38 @@ TEST_F(TaskTest, shouldReturnRightTaskLabel){
     Task task_without_label =
             Task::Create("title", Task::Priority::HIGH, time(0));
     Task task_with_label =
-            Task::Create("title", Task::Priority::HIGH, time(0), expected2);
+            Task::Create("title", Task::Priority::HIGH, time(0), false, expected2);
     // Act
     std::string acutal1 = task_without_label.GetLabel();
     std::string actual2 = task_with_label.GetLabel();
     // Asset
     EXPECT_EQ(expected1, acutal1);
     EXPECT_EQ(expected2, actual2);
+}
+
+TEST_F(TaskTest, OperatorEqualsShouldReturnTrueForEqualTasks){
+    // Arrange
+    const time_t some_time = time(0);
+    Task task1 = Task::Create("title", Task::Priority::NONE, some_time);
+    Task task2 = Task::Create("title", Task::Priority::NONE, some_time);
+    // Act & Assert
+    EXPECT_TRUE(task1 == task2);
+    EXPECT_TRUE(task2 == task1);
+}
+
+TEST_F(TaskTest, OperatorEqualsShouldReturnFalseForNotEqualTasks){
+    // Arrange
+    const time_t some_time = time(0);
+    Task task1 = Task::Create("title", Task::Priority::NONE, some_time);
+    Task task2 = Task::Create("title2", Task::Priority::NONE, some_time);
+    Task task3 = Task::Create("title", Task::Priority::LOW, some_time);
+    Task task4 = Task::Create("title", Task::Priority::NONE, some_time + 5);
+    Task task5 = Task::Create("title", Task::Priority::NONE, some_time, true);
+    Task task6 = Task::Create("title", Task::Priority::NONE, some_time, false, "some label");
+    // Act & Assert
+    EXPECT_FALSE(task1 == task2);
+    EXPECT_FALSE(task1 == task3);
+    EXPECT_FALSE(task1 == task4);
+    EXPECT_FALSE(task1 == task5);
+    EXPECT_FALSE(task1 == task6);
 }
