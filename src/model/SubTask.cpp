afa47bf9fe5d06a2427455341560755a76bb654e
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-21 17:41:06 +0200

    added SubTask constructor and Create method

--- src/model/SubTask.cpp
+++ src/model/SubTask.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "SubTask.h"
+
+TaskId SubTask::GetParentTaskId() {
+    return Task::GetParentTaskId();
+}
+
+SubTask SubTask::Create(const std::string &title,
+                        Task::Priority priority,
+                        time_t due_to,
+                        const TaskId &parent,
+                        bool completed,
+                        const std::string &label) {
+    return SubTask(title, priority, due_to, parent, completed, label);
+}
+
+SubTask::SubTask(const std::string &title,
+                 Task::Priority priority,
+                 time_t due_to,
+                 const TaskId &parent,
+                 bool completed,
+                 const std::string &label) : Task(title, priority, due_to, completed, label), parent_(parent) {
+
+}
--- src/model/SubTask.h
+++ src/model/SubTask.h
@@ -8,7 +8,24 @@
 #include "Task.h"
 
 class SubTask : public Task {
-
+public:
+    static SubTask Create(const std::string& title,
+                       Priority priority,
+                       time_t due_to,
+                       const TaskId& parent,
+                       bool completed = false,
+                       const std::string& label = std::string(""));
+public:
+    TaskId GetParentTaskId() override;
+private:
+    SubTask(const std::string& title,
+         Priority priority,
+         time_t due_to,
+         const TaskId& parent,
+         bool completed = false,
+         const std::string& label = std::string(""));
+private:
+    TaskId parent_;
 };
 
 
