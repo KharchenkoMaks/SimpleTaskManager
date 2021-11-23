912edf0700da0f4f2d4302a5aa3f1941aac1df3d
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-23 16:36:38 +0200

    added write methods to ConsolePrinter, prints message to console

--- src/view/printer/ConsolePrinter.cpp
+++ src/view/printer/ConsolePrinter.cpp
@@ -11,3 +11,15 @@ ConsolePrinter::ConsolePrinter() {
 ConsolePrinter ConsolePrinter::Create() {
     return ConsolePrinter();
 }
+
+void ConsolePrinter::Write(const std::string &message) {
+    std::cout << message;
+}
+
+void ConsolePrinter::WriteLine(const std::string &message) {
+    Write(message + "\n");
+}
+
+void ConsolePrinter::WriteError(const std::string &message) {
+    WriteLine("[Error]: " + message);
+}
--- src/view/printer/ConsolePrinter.h
+++ src/view/printer/ConsolePrinter.h
@@ -5,10 +5,16 @@
 #ifndef SIMPLETASKMANAGER_CONSOLEPRINTER_H
 #define SIMPLETASKMANAGER_CONSOLEPRINTER_H
 
+#include <iostream>
+#include <string>
 
 class ConsolePrinter {
 public:
     static ConsolePrinter Create();
+public:
+    void Write(const std::string& message);
+    void WriteLine(const std::string& message);
+    void WriteError(const std::string& message);
 private:
     ConsolePrinter();
 };
