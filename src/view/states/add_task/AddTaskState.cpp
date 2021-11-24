3061ada3022ffb854c8b3ca4139a6fd8ef816b77
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-24 19:14:19 +0200

    added AddTaskState realization

--- src/view/states/add_task/AddTaskState.cpp
+++ src/view/states/add_task/AddTaskState.cpp
@@ -7,5 +7,20 @@
 std::shared_ptr<IWizardState>
 AddTaskState::Execute(std::shared_ptr<WizardContext> context,
                       std::shared_ptr<WizardStatesFactory> state_factory) {
+    // TODO (Maksym): make ConsoleStateMachine instead of this while loop
 
+    std::shared_ptr<WizardStatesFactory> new_factory = std::make_shared<WizardStatesFactory>();
+    std::shared_ptr<WizardContext> new_context = std::make_shared<WizardContext>();
+    std::shared_ptr<IWizardState> new_state =
+            new_factory->GetState(WizardStatesFactory::States::kInputTaskTitle);
+
+    while (!new_context->IsStateMachineStopped()){
+        new_state = new_state->Execute(new_context, new_factory);
+    }
+
+    if (new_context->GetAddedTask().has_value()) {
+        context->AddTask(new_context->GetAddedTask());
+    } else {
+        throw std::runtime_error("Task adding failed.");
+    }
 }
