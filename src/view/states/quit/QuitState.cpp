839428186f08618d2945990d1945d3fde28f561c
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-24 17:13:12 +0200

    added user confirmation for quit command

--- src/view/states/quit/QuitState.cpp
+++ src/view/states/quit/QuitState.cpp
@@ -6,6 +6,19 @@
 
 std::shared_ptr<IWizardState> QuitState::Execute(std::shared_ptr<WizardContext> context,
                                                  std::shared_ptr<WizardStatesFactory> state_factory) {
-    context->QuitWizard();
+
+    std::shared_ptr<WizardStatesFactory> new_state_factory = std::make_shared<WizardStatesFactory>();
+    std::shared_ptr<IWizardState> new_state = new_state_factory->GetConfirmState();
+    std::shared_ptr<WizardContext> new_context = std::make_shared<WizardContext>();
+    new_context->SetConfirmMessage("Are you sure you want to quit? No changes will be saved!");
+    while (!new_context->IsWizardEnded()) {
+        new_state = new_state->Execute(new_context, new_state_factory);
+    }
+    if (new_context->GetConfirmationStatus() == WizardContext::ConfirmationStatus::kConfirmed) {
+        context->QuitWizard();
+        return state_factory->GetRootState();
+    } else if (new_context->GetConfirmationStatus() == WizardContext::ConfirmationStatus::kNone) {
+        throw std::runtime_error("Confirmation process was failed.");
+    }
     return state_factory->GetRootState();
 }
