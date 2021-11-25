a3ac7d9bf21fbd3851f95fcf671b565704c37195
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-25 16:48:19 +0200

    reworked quit confirmation

--- src/view/states/quit/QuitState.cpp
+++ src/view/states/quit/QuitState.cpp
@@ -6,6 +6,20 @@
 
 std::optional<std::shared_ptr<IWizardState>> QuitState::Execute(std::shared_ptr<WizardContext> context,
                                                  std::shared_ptr<WizardStatesFactory> state_factory) {
+    if (UserConfirm()) {
+        return std::nullopt;
+    } else {
+        return state_factory->GetState(WizardStatesFactory::States::kQuit);
+    }
+}
 
+bool QuitState::UserConfirm() {
+    printer_->WriteLine("Are you sure you want to quit?");
+    printer_->Write("Y/N> ");
+    std::string users_answer = printer_->ReadLine();
+    return users_answer == "Y" || users_answer == "y";
+}
+
+QuitState::QuitState(const std::shared_ptr<ConsolePrinter>& printer) : WizardStatePrinter(printer) {
 
 }
--- src/view/states/quit/QuitState.h
+++ src/view/states/quit/QuitState.h
@@ -5,16 +5,21 @@
 #ifndef SIMPLETASKMANAGER_QUITSTATE_H
 #define SIMPLETASKMANAGER_QUITSTATE_H
 
-#include "states/abstract/IWizardState.h"
+#include "states/abstract/WizardStatePrinter.h"
 #include "states/factory/WizardStatesFactory.h"
 #include "context/WizardContext.h"
 
 #include <memory>
+#include <string>
 
-class QuitState : public IWizardState {
+class QuitState : public WizardStatePrinter {
+public:
+    explicit QuitState(const std::shared_ptr<ConsolePrinter>& printer);
 public:
     std::optional<std::shared_ptr<IWizardState>> Execute(std::shared_ptr<WizardContext> context,
                                           std::shared_ptr<WizardStatesFactory> state_factory) override;
+private:
+    bool UserConfirm();
 };
 
 
