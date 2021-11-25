b2a4447499bbc9de0fe6b039d00cdd0fedef3396
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-25 16:34:26 +0200

    removed ConfirmState

--- src/view/states/quit/QuitState.cpp
+++ src/view/states/quit/QuitState.cpp
@@ -7,27 +7,5 @@
 std::shared_ptr<IWizardState> QuitState::Execute(std::shared_ptr<WizardContext> context,
                                                  std::shared_ptr<WizardStatesFactory> state_factory) {
 
-    // TODO (Maksym): make ConsoleStateMachine instead of this while
-
-    std::shared_ptr<WizardStatesFactory> new_state_factory = std::make_shared<WizardStatesFactory>();
-    std::shared_ptr<IWizardState> new_state =
-            new_state_factory->GetState(WizardStatesFactory::States::kConfirm);
-    std::shared_ptr<WizardContext> new_context = std::make_shared<WizardContext>();
-    new_context->SetConfirmMessage("Are you sure you want to quit? No changes will be saved!");
-    while (!new_context->IsStateMachineStopped()) {
-        new_state = new_state->Execute(new_context, new_state_factory);
-    }
-
-    switch (new_context->GetConfirmationStatus()) {
-        case WizardContext::ConfirmationStatus::kConfirmed: {
-            context->InterruptStateMachine();
-            return state_factory->GetState(WizardStatesFactory::States::kQuit);
-        }
-        case WizardContext::ConfirmationStatus::kNotConfirmed: {
-            return state_factory->GetState(WizardStatesFactory::States::kRoot);
-        }
-        case WizardContext::ConfirmationStatus::kNone: {
-            throw std::runtime_error("Confirmation process was failed.");
-        }
-    }
+    
 }
