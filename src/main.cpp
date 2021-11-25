7fec7d9736ada7049e6c6bd6e58478a694bf0baa
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-25 15:22:18 +0200

    added TODO list of fixes

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,16 @@ int main() {
     state_machine.Run(WizardStatesFactory::States::kRoot);
     return 0;
 }
+
+// TODO (Maksym): Make context inside StateMachine unique
+// TODO: Move factory, context out of state machine
+// TODO: Get rid of ConfirmState
+// TODO: Move next steps logic to states factory
+// TODO: Make lazy states initialization in states factory
+// TODO: Use state machine inside AddTaskState and EditTaskState
+// TODO: Make abstract State for states that run state machine inside?
+// TODO: Rename WizardStatePrinter abstract class
+// TODO: Rename Printer to PrinterWithScanner =)
+// TODO: Make default methods in WizardStatePrinter to ask user for some input
+// TODO: Delete unnecessary directories
+// TODO: Change subtasks structure
