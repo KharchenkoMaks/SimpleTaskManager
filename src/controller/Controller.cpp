1198c4bd1cf5ab75d6d8a7f81cda5ae1f20bd821
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-29 17:21:37 +0200

    implemented basic controller

--- src/controller/Controller.cpp
+++ src/controller/Controller.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "Controller.h"
+
+Controller::Controller(std::unique_ptr<TaskManager> task_manager,
+                       std::unique_ptr<TaskValidator> task_validator) :
+                       task_manager_(std::move(task_manager)),
+                       task_validator_(std::move(task_validator)) {
+
+}
+
+TaskId Controller::AddTask(const Task &task) {
+    if (task_validator_->ValidateTask(task)){
+        return task_manager_->Create(task);
+    } else {
+        throw std::invalid_argument("Invalid task was given.");
+    }
+}
+
+void Controller::EditTask(const TaskId &task_id, const Task &task) {
+    if (task_validator_->ValidateTask(task) && task_validator_->ValidateTaskId(task_id)) {
+        if (task_manager_->IsTaskIdExist(task_id)) {
+            task_manager_->Edit(task_id, task);
+        }
+    } else {
+        throw std::invalid_argument("Invalid task or task_id was given.");
+    }
+}
+
+void Controller::DeleteTask(const TaskId &task_id) {
+    if (task_validator_->ValidateTaskId(task_id)) {
+        if (task_manager_->IsTaskIdExist(task_id)){
+            task_manager_->Delete(task_id);
+        }
+    } else {
+        throw std::invalid_argument("Invalid task_id was given.");
+    }
+}
+
+void Controller::CompleteTask(const TaskId &task_id) {
+    if (task_validator_->ValidateTaskId(task_id)) {
+        if (task_manager_->IsTaskIdExist(task_id)){
+            task_manager_->Complete(task_id);
+        }
+    } else {
+        throw std::invalid_argument("Invalid task_id was given.");
+    }
+}
--- src/controller/Controller.h
+++ src/controller/Controller.h
@@ -5,12 +5,25 @@
 #ifndef SIMPLETASKMANAGER_CONTROLLER_H
 #define SIMPLETASKMANAGER_CONTROLLER_H
 
-#include "ConsoleStateMachine.h"
-#include "states/factory/WizardStatesFactory.h"
+#include "TaskId.h"
+#include "tasks/Task.h"
+#include "TaskManager.h"
+#include "TaskValidator.h"
+
+#include <memory>
 
 class Controller {
+public:
+    Controller(std::unique_ptr<TaskManager> task_manager, std::unique_ptr<TaskValidator> task_validator);
+public:
+    TaskId AddTask(const Task& task);
+    void EditTask(const TaskId& task_id, const Task& task);
+    void DeleteTask(const TaskId& task_id);
+    void CompleteTask(const TaskId& task_id);
 private:
+    std::unique_ptr<TaskManager> task_manager_;
 
+    std::unique_ptr<TaskValidator> task_validator_;
 };
 
 
