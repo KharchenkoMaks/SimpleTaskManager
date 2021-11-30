51ed0e1404b9d21a50d17cfb4a532193521623f6
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-11-30 16:21:42 +0200

    implemented DueTime class

---
+++ src/model/DueTime.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Maksym Kharchenko on 30.11.2021.
+//
+
+#include "DueTime.h"
+
+DueTime DueTime::Create(const std::string& due_time, const std::string& format = "%H:%M %d.%m.%Y") {
+    return DueTime(due_time, format);
+}
+
+DueTime::DueTime(const std::string& due_time, const std::string& format) {
+    due_time_ = StringToTime(due_time, format);
+}
+
+time_t DueTime::StringToTime(const std::string& time_string, const std::string& format = "%H:%M %d.%m.%Y") const {
+    struct std::tm tm;
+    std::istringstream ss(time_string);
+    ss >> std::get_time(&tm, format.c_str());
+    time_t some_time = mktime(&tm);
+    return some_time;
+}
+
+std::string DueTime::TimeToString(time_t time, const std::string& format = "%H:%M %d.%m.%Y") const {
+    char buffer[32];
+    std::tm *t = std::localtime(&time);
+    std::strftime(buffer, 32, format.c_str(), t);
+    return buffer;
+}
+
+std::string DueTime::GetTimeString(const std::string& format = "%H:%M %d.%m.%Y") const {
+    return TimeToString(due_time_, format);
+}
+
+time_t DueTime::GetTime() const {
+    return due_time_;
+}
---
+++ src/model/DueTime.h
@@ -0,0 +1,29 @@
+//
+// Created by Maksym Kharchenko on 30.11.2021.
+//
+
+#ifndef SIMPLETASKMANAGER_DUETIME_H
+#define SIMPLETASKMANAGER_DUETIME_H
+
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+class DueTime {
+public:
+    static DueTime Create(const std::string& due_time, const std::string& format);
+public:
+    std::string GetTimeString(const std::string& format) const;
+    time_t GetTime() const;
+private:
+    DueTime(const std::string& due_time, const std::string& format);
+private:
+    time_t StringToTime(const std::string& time_string, const std::string& format) const;
+    std::string TimeToString(time_t time, const std::string& format) const;
+private:
+    time_t due_time_;
+};
+
+
+#endif //SIMPLETASKMANAGER_DUETIME_H
