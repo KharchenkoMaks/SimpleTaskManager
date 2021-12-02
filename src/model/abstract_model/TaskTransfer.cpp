a391f5b65ef19cb1b7a0ba533982f7b0b4e418c2
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-12-02 13:47:45 +0200

    implemented TaskTransfer class, used to be returned in IModel API

--- src/model/abstract_model/TaskTransfer.cpp
+++ src/model/abstract_model/TaskTransfer.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "TaskTransfer.h"
+
+TaskTransfer::TaskTransfer(const TaskId& task_id,
+                           const Task& task,
+                           const std::optional<TaskId>& parent_id) :
+                           task_id_(task_id),
+                           task_(task),
+                           parent_id_(parent_id) {
+
+}
+
+TaskTransfer TaskTransfer::Create(const TaskId& task_id, const Task& task, const std::optional<TaskId>& parent_id) {
+    return TaskTransfer(task_id, task, parent_id);
+}
+
+TaskId TaskTransfer::GetTaskId() const {
+    return task_id_;
+}
+
+Task TaskTransfer::GetTask() const {
+    return task_;
+}
+
+std::optional<TaskId> TaskTransfer::GetParentTaskId() const {
+    return parent_id_;
+}
--- src/model/abstract_model/TaskTransfer.h
+++ src/model/abstract_model/TaskTransfer.h
@@ -5,9 +5,28 @@
 #ifndef SIMPLETASKMANAGER_TASKTRANSFER_H
 #define SIMPLETASKMANAGER_TASKTRANSFER_H
 
+#include "tasks/Task.h"
+#include "TaskId.h"
+
+#include <optional>
 
 class TaskTransfer {
-
+public:
+    static TaskTransfer Create(const TaskId& task_id,
+                               const Task& task,
+                               const std::optional<TaskId>& parent_id);
+public:
+    TaskId GetTaskId() const;
+    Task GetTask() const;
+    std::optional<TaskId> GetParentTaskId() const;
+private:
+    TaskTransfer(const TaskId& task_id,
+                 const Task& task,
+                 const std::optional<TaskId>& parent_id);
+private:
+    TaskId task_id_;
+    Task task_;
+    std::optional<TaskId> parent_id_;
 };
 
 
