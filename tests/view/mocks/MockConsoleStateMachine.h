4c3a388b67e3ebaa932a661732dce94965063657
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-12-07 11:36:35 +0200

    added mock for ConsoleStateMachine

---
+++ tests/view/mocks/MockConsoleStateMachine.h
@@ -0,0 +1,18 @@
+//
+// Created by Maksym Kharchenko on 07.12.2021.
+//
+
+#ifndef SIMPLETASKMANAGER_MOCKCONSOLESTATEMACHINE_H
+#define SIMPLETASKMANAGER_MOCKCONSOLESTATEMACHINE_H
+
+#include "ConsoleStateMachine.h"
+
+class MockConsoleStateMachine : public ConsoleStateMachine {
+public:
+    MOCK_METHOD(void,
+                Run,
+                (const std::shared_ptr<WizardContext>& context, const std::optional<std::shared_ptr<WizardStateConsole>>& initial_state),
+                (override));
+};
+
+#endif //SIMPLETASKMANAGER_MOCKCONSOLESTATEMACHINE_H
