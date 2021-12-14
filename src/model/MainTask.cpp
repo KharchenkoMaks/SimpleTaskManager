f69360c18e222585a6ee3b9f46bb4c568073be48
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2021-12-14 15:44:55 +0200

    implemented MainTask class

--- src/model/MainTask.cpp
+++ src/model/MainTask.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "MainTask.h"
+
+MainTask::MainTask(const Task& task) {
+    task_.CopyFrom(task);
+}
+
+MainTask MainTask::Create(const Task& task) {
+    return MainTask(task);
+}
+
+void MainTask::AddSubTask(const TaskId& task_id, const Task& subtask) {
+    subtasks_.insert_or_assign(task_id, subtask);
+}
+
+bool MainTask::ContainsSubTask(const TaskId& subtask_id) {
+    return subtasks_.find(subtask_id) != subtasks_.end();
+}
--- src/model/MainTask.h
+++ src/model/MainTask.h
@@ -5,12 +5,25 @@
 #ifndef SIMPLETASKMANAGER_MAINTASK_H
 #define SIMPLETASKMANAGER_MAINTASK_H
 
+#include "Task.pb.h"
+#include "utilities/TaskIdUtilities.h"
 
+#include <map>
+#include <utility>
 
 class MainTask {
-
+public:
+    static MainTask Create(const Task& task);
+public:
+    void AddSubTask(const TaskId& task_id, const Task& subtask);
+public:
+    bool ContainsSubTask(const TaskId& subtask_id);
+private:
+    explicit MainTask(const Task& task);
+public:
+    Task task_;
+    std::map<TaskId, Task> subtasks_;
 };
-
 
 
 #endif //SIMPLETASKMANAGER_MAINTASK_H
