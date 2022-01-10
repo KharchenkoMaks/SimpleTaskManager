4753a13895ddf7898d1fc1e74283ba894c2fe10b
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2022-01-10 14:06:42 +0200

    reworked integrational test launching

--- tests/IntegrationTests.cpp
+++ tests/IntegrationTests.cpp
@@ -18,55 +18,65 @@ using ::testing::Return;
 using ::testing::InSequence;
 
 class IntegrationTests : public ::testing::Test {
+public:
+    void LaunchTest(const std::string& expected_show,
+                    const std::vector<std::string>& inputs) {
+        std::unique_ptr<MockConsolePrinter> printer_ = std::make_unique<MockConsolePrinter>();
+        std::unique_ptr<MockConsoleReader> reader_ = std::make_unique<MockConsoleReader>();
+        {
+            InSequence s;
+            for (const std::string &input: inputs) {
+                EXPECT_CALL(*reader_, ReadLine()).WillOnce(Return(input));
+            }
+        }
+        EXPECT_CALL(*printer_, WriteError(testing::_)).Times(testing::AnyNumber());
+        EXPECT_CALL(*printer_, Write(testing::_)).Times(testing::AnyNumber());
 
+        EXPECT_CALL(*printer_, WriteLine(testing::_)).Times(testing::AnyNumber());
+        EXPECT_CALL(*printer_, WriteLine(expected_show)).Times(1);
+        // Arrange
+        std::shared_ptr<CommandFactory> command_factory = std::make_shared<CommandFactory>();
+        std::shared_ptr<StatesFactory> states_factory = std::make_shared<StatesFactory>(command_factory,
+                                                                                        std::move(printer_),
+                                                                                        std::move(reader_));
+        std::unique_ptr<UserInterface> user_interface = std::make_unique<UserInterface>(
+                states_factory);
+        std::unique_ptr<Controller> model_controller = std::make_unique<Controller>(
+                std::make_unique<TaskManager>(std::make_unique<IdGenerator>()),
+                std::make_unique<TaskValidator>());
+        std::unique_ptr<ViewController> view_controller = std::make_unique<ViewController>(
+                std::move(model_controller),
+                std::move(user_interface));
+        // Act
+        view_controller->RunUserInterface();
+    }
 };
 
 TEST_F(IntegrationTests, Script1) {
     // Arrange
     std::string expected_show = "ID: 0, SomeTask, Priority: High, Due to: 16:30 01.01.2023, Completed: Yes";
-    // Assert
-    std::unique_ptr<MockConsolePrinter> printer_ = std::make_unique<MockConsolePrinter>();
-    std::unique_ptr<MockConsoleReader> reader_ = std::make_unique<MockConsoleReader>();
-
-    EXPECT_CALL(*reader_, ReadLine())
-            .WillOnce(Return("abc"))
-            .WillOnce(Return("complete"))
-            .WillOnce(Return("1"))
-            .WillOnce(Return(""))
-            .WillOnce(Return("add"))
-            .WillOnce(Return("SomeTask"))
-            .WillOnce(Return("Ne xo4y"))
-            .WillOnce(Return("4"))
-            .WillOnce(Return("High"))
-            .WillOnce(Return("15:00"))
-            .WillOnce(Return("16:30 01.01.2023"))
-            .WillOnce(Return("complete"))
-            .WillOnce(Return("5"))
-            .WillOnce(Return("y"))
-            .WillOnce(Return("complete"))
-            .WillOnce(Return("0"))
-            .WillOnce(Return("y"))
-            .WillOnce(Return("show"))
-            .WillOnce(Return("quit"))
-            .WillOnce(Return("y"));
-    EXPECT_CALL(*printer_, WriteError(testing::_)).Times(testing::AnyNumber());
-    EXPECT_CALL(*printer_, Write(testing::_)).Times(testing::AnyNumber());
-
-    EXPECT_CALL(*printer_, WriteLine(testing::_)).Times(testing::AnyNumber());
-    EXPECT_CALL(*printer_, WriteLine(expected_show)).Times(1);
-    // Arrange
-    std::shared_ptr<CommandFactory> command_factory = std::make_shared<CommandFactory>();
-    std::shared_ptr<StatesFactory> states_factory = std::make_shared<StatesFactory>(command_factory,
-                                                                                    std::move(printer_),
-                                                                                    std::move(reader_));
-    std::unique_ptr<UserInterface> user_interface = std::make_unique<UserInterface>(
-            states_factory);
-    std::unique_ptr<Controller> model_controller = std::make_unique<Controller>(
-            std::make_unique<TaskManager>(std::make_unique<IdGenerator>()),
-            std::make_unique<TaskValidator>());
-    std::unique_ptr<ViewController> view_controller = std::make_unique<ViewController>(
-            std::move(model_controller),
-            std::move(user_interface));
+    std::vector<std::string> inputs = {
+            "qwe",
+            "complete",
+            "1",
+            "",
+            "add",
+            "SomeTask",
+            "Ne xo4y",
+            "4",
+            "High",
+            "15:00",
+            "16:30 01.01.2023",
+            "complete",
+            "5",
+            "y",
+            "complete",
+            "0",
+            "y",
+            "show",
+            "quit",
+            "y"
+    };
     // Act
-    view_controller->RunUserInterface();
+    LaunchTest(expected_show, inputs);
 }
