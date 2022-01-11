8bf54cb8fb2a57d643c3ee47e31e73cdae3fb964
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2022-01-11 16:04:05 +0200

    implemented Controller mock

---
+++ tests/view/CommandsTests.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Maksym Kharchenko on 11.01.2022.
+//
+
--- tests/view/mocks/MockController.h
+++ tests/view/mocks/MockController.h
@@ -19,12 +19,18 @@ class MockController : public Controller {
 public:
     MockController() : Controller(nullptr, nullptr) {}
 
+    MOCK_METHOD((std::pair<ControllerRequestResult, TaskId>), AddTask, (const Task& task), (override));
+    MOCK_METHOD((std::pair<ControllerRequestResult, TaskId>), AddSubTask, (const Task& task, const TaskId& parent_id), (override));
+    MOCK_METHOD(ControllerRequestResult, EditTask, (const TaskId& task_id, const Task& task), (override));
+    MOCK_METHOD(ControllerRequestResult, DeleteTask, (const TaskId& task_id), (override));
+    MOCK_METHOD(ControllerRequestResult, CompleteTask, (const TaskId& task_id), (override));
+    MOCK_METHOD(ControllerRequestResult, SetTaskLabel, (const TaskId& task_id, const std::string& label), (override));
+    MOCK_METHOD(ControllerRequestResult, DeleteTaskWithSubTasks, (const TaskId& task_id), (override));
+    MOCK_METHOD(ControllerRequestResult, CompleteTaskWithSubTasks, (const TaskId& task_id), (override));
     MOCK_METHOD(std::vector<TaskTransfer>, GetAllTasks, (), (override));
-    MOCK_METHOD((std::pair<TaskActionResult, std::optional<TaskId>>), AddTask, (const Task& task), (override));
-    MOCK_METHOD((std::pair<TaskActionResult, std::optional<TaskId>>), AddSubTask, (const Task& task, const TaskId& parent_id), (override));
-    MOCK_METHOD(TaskActionResult, DeleteTask, (const TaskId& task_id), (override));
-    MOCK_METHOD(persistence::SaveLoadStatus, SaveToFile, (const std::string& file_name), (override));
-    MOCK_METHOD(persistence::SaveLoadStatus, LoadFromFile, (const std::string& file_name), (override));
+    MOCK_METHOD(std::optional<TaskTransfer>, GetTask, (const TaskId& task_id), (override));
+    MOCK_METHOD(ControllerRequestResult, SaveToFile, (const std::string& file_name), (override));
+    MOCK_METHOD(ControllerRequestResult, LoadFromFile, (const std::string& file_name), (override));
 };
 
 #endif //SIMPLETASKMANAGER_MOCKCONTROLLER_H
