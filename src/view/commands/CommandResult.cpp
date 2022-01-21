30b68925fdb01dacb54388be1c02604682a682f8
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2022-01-21 14:19:12 +0200

    reworked TasksToShow in command result, now contains information about showing task relations

--- src/view/commands/CommandResult.cpp
+++ src/view/commands/CommandResult.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "CommandResult.h"
+#include "utilities/TaskComparators.h"
 
 CommandResult CommandResult::Create(const ControllerRequestResult result) {
     CommandResult cmd_result;
@@ -17,10 +18,11 @@ CommandResult CommandResult::Create(const TaskId& task_id) {
     return cmd_result;
 }
 
-CommandResult CommandResult::Create(const std::vector<RelationalTask>& tasks_to_show) {
+CommandResult CommandResult::Create(const std::vector<RelationalTask>& tasks_to_show, const bool show_tasks_relations) {
     CommandResult cmd_result;
     cmd_result.result_ = ControllerRequestResult::SUCCESS;
-    cmd_result.tasks_to_show_ = tasks_to_show;
+    cmd_result.tasks_to_show_.tasks_ = tasks_to_show;
+    cmd_result.tasks_to_show_.show_task_relations_ = show_tasks_relations;
     return cmd_result;
 }
 
@@ -32,6 +34,11 @@ std::optional<TaskId> CommandResult::GetTaskId() const {
     return task_id_;
 }
 
-std::vector<RelationalTask> CommandResult::GetTasksToShow() const {
+CommandResult::TasksToShow CommandResult::GetTasksToShow() const {
     return tasks_to_show_;
+}
+
+bool CommandResult::TasksToShow::operator==(const CommandResult::TasksToShow& tasks_to_show) const {
+    return this->tasks_ == tasks_to_show.tasks_ &&
+           this->show_task_relations_ == tasks_to_show.show_task_relations_;
 }
