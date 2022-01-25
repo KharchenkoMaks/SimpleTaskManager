3e7000568d76d0c3df1c33cb4965cd111fc7ce47
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2022-01-25 12:07:03 +0200

    implemented GRPCServerEndPoint, receives requests and give them to DefaultModelController

--- src/model/GRPCServerEndPoint.cpp
+++ src/model/GRPCServerEndPoint.cpp
@@ -3,3 +3,127 @@
 //
 
 #include "GRPCServerEndPoint.h"
+
+GRPCServerEndPoint::GRPCServerEndPoint(std::unique_ptr<ModelController> model_controller) :
+            model_controller_(std::move(model_controller)) {}
+
+grpc::Status GRPCServerEndPoint::AddTask(::grpc::ServerContext *context,
+                                         const ::AddTaskRequest *request,
+                                         ::AddTaskResponse *response) {
+    auto controller_result = model_controller_->AddTask(request->task());
+    response->set_status(ConvertToControllerRequestStatus(controller_result.first));
+    response->set_allocated_added_task_id(new TaskId(controller_result.second));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::AddSubTask(::grpc::ServerContext *context,
+                                            const ::AddSubTaskRequest *request,
+                                            ::AddSubTaskResponse *response) {
+    auto controller_result = model_controller_->AddSubTask(request->task(), request->parent_id());
+    response->set_status(ConvertToControllerRequestStatus(controller_result.first));
+    response->set_allocated_added_task_id(new TaskId(controller_result.second));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::EditTask(::grpc::ServerContext *context,
+                                          const ::EditTaskRequest *request,
+                                          ::EditTaskResponse *response) {
+    auto controller_result = model_controller_->EditTask(request->task_id(), request->task());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::DeleteTask(::grpc::ServerContext *context,
+                                            const ::DeleteTaskRequest *request,
+                                            ::DeleteTaskResponse *response) {
+    auto controller_result = model_controller_->DeleteTask(request->task_id());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::CompleteTask(::grpc::ServerContext *context,
+                                            const ::CompleteTaskRequest *request,
+                                            ::CompleteTaskResponse *response) {
+    auto controller_result = model_controller_->CompleteTask(request->task_id());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::AddTaskLabel(::grpc::ServerContext *context,
+                                              const ::AddTaskLabelRequest *request,
+                                              ::AddTaskLabelResponse *response) {
+    auto controller_result = model_controller_->AddTaskLabel(request->task_id(), request->label());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::DeleteTaskWithSubTasks(::grpc::ServerContext *context,
+                                                        const ::DeleteTaskWithSubTasksRequest *request,
+                                                        ::DeleteTaskWithSubTasksResponse *response) {
+    auto controller_result = model_controller_->DeleteTaskWithSubTasks(request->task_id());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::CompleteTaskWithSubTasks(::grpc::ServerContext *context,
+                                                        const ::CompleteTaskWithSubTasksRequest *request,
+                                                        ::CompleteTaskWithSubTasksResponse *response) {
+    auto controller_result = model_controller_->CompleteTaskWithSubTasks(request->task_id());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::GetAllTasks(::grpc::ServerContext *context,
+                                             const ::GetAllTasksRequest *request,
+                                             ::GetAllTasksResponse *response) {
+    auto controller_result = model_controller_->GetAllTasks();
+    for (const auto& task : controller_result) {
+        response->mutable_tasks()->AddAllocated(new RelationalTask(task));
+    }
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::GetTask(::grpc::ServerContext *context,
+                                         const ::GetTaskRequest *request,
+                                         ::GetTaskResponse *response) {
+    auto controller_result = model_controller_->GetTask(request->task_id());
+    if (controller_result.has_value())
+        response->set_allocated_task(new RelationalTask(controller_result.value()));
+
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::SaveToFile(::grpc::ServerContext *context,
+                                            const ::SaveToFileRequest *request,
+                                            ::SaveToFileResponse *response) {
+    auto controller_result = model_controller_->SaveToFile(request->file_name());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+grpc::Status GRPCServerEndPoint::LoadFromFile(::grpc::ServerContext *context,
+                                              const ::LoadFromFileRequest *request,
+                                              ::LoadFromFileResponse *response) {
+    auto controller_result = model_controller_->LoadFromFile(request->file_name());
+    response->set_status(ConvertToControllerRequestStatus(controller_result));
+    return grpc::Status::OK;
+}
+
+ControllerRequestStatus GRPCServerEndPoint::ConvertToControllerRequestStatus(const ControllerRequestResult& controller_result) const {
+    switch (controller_result) {
+        case ControllerRequestResult::SUCCESS:
+            return ControllerRequestStatus::SUCCESS;
+        case ControllerRequestResult::FAIL_NO_SUCH_TASK:
+            return ControllerRequestStatus::FAIL_NO_SUCH_TASK;
+        case ControllerRequestResult::FAIL_UNCOMPLETED_SUBTASKS:
+            return ControllerRequestStatus::FAIL_UNCOMPLETED_SUBTASKS;
+        case ControllerRequestResult::FAIL_NOT_DELETED_SUBTASKS:
+            return ControllerRequestStatus::FAIL_NOT_DELETED_SUBTASKS;
+        case ControllerRequestResult::FAIL_INVALID_TASK:
+            return ControllerRequestStatus::FAIL_INVALID_TASK;
+        case ControllerRequestResult::FILE_WAS_NOT_OPENED:
+            return ControllerRequestStatus::FILE_WAS_NOT_OPENED;
+        case ControllerRequestResult::FILE_DAMAGED:
+            return ControllerRequestStatus::FILE_DAMAGED;
+    }
+}
--- src/model/GRPCServerEndPoint.h
+++ src/model/GRPCServerEndPoint.h
@@ -5,12 +5,39 @@
 #ifndef SIMPLETASKMANAGER_GRPCSERVERENDPOINT_H
 #define SIMPLETASKMANAGER_GRPCSERVERENDPOINT_H
 
+#include "controller/ModelController.h"
 
+#include "Requests.pb.h"
+#include "Responses.pb.h"
+#include "ModelControllerService.grpc.pb.h"
 
-class GRPCServerEndPoint {
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
 
+class GRPCServerEndPoint final : public ModelControllerService::Service {
+public:
+    explicit GRPCServerEndPoint(std::unique_ptr<ModelController> model_controller);
+
+public:
+    grpc::Status AddTask(::grpc::ServerContext *context, const ::AddTaskRequest *request, ::AddTaskResponse *response) override;
+    grpc::Status AddSubTask(::grpc::ServerContext *context, const ::AddSubTaskRequest *request, ::AddSubTaskResponse *response) override;
+    grpc::Status EditTask(::grpc::ServerContext *context, const ::EditTaskRequest *request, ::EditTaskResponse *response) override;
+    grpc::Status DeleteTask(::grpc::ServerContext *context, const ::DeleteTaskRequest *request, ::DeleteTaskResponse *response) override;
+    grpc::Status CompleteTask(::grpc::ServerContext *context, const ::CompleteTaskRequest *request, ::CompleteTaskResponse *response) override;
+    grpc::Status AddTaskLabel(::grpc::ServerContext *context, const ::AddTaskLabelRequest *request, ::AddTaskLabelResponse *response) override;
+    grpc::Status DeleteTaskWithSubTasks(::grpc::ServerContext *context, const ::DeleteTaskWithSubTasksRequest *request, ::DeleteTaskWithSubTasksResponse *response) override;
+    grpc::Status CompleteTaskWithSubTasks(::grpc::ServerContext *context, const ::CompleteTaskWithSubTasksRequest *request, ::CompleteTaskWithSubTasksResponse *response) override;
+    grpc::Status GetAllTasks(::grpc::ServerContext *context, const ::GetAllTasksRequest *request, ::GetAllTasksResponse *response) override;
+    grpc::Status GetTask(::grpc::ServerContext *context, const ::GetTaskRequest *request, ::GetTaskResponse *response) override;
+    grpc::Status SaveToFile(::grpc::ServerContext *context, const ::SaveToFileRequest *request, ::SaveToFileResponse *response) override;
+    grpc::Status LoadFromFile(::grpc::ServerContext *context, const ::LoadFromFileRequest *request, ::LoadFromFileResponse *response) override;
+
+private:
+    ControllerRequestStatus ConvertToControllerRequestStatus(const ControllerRequestResult& controller_result) const;
+private:
+    std::unique_ptr<ModelController> model_controller_;
 };
-
 
 
 #endif //SIMPLETASKMANAGER_GRPCSERVERENDPOINT_H
