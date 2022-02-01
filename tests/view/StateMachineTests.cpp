2c281cb91c793af24d0c7b10699d7cb54542fa40
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2022-02-01 15:09:18 +0200

    fixed state machine tests

--- tests/view/StateMachineTests.cpp
+++ tests/view/StateMachineTests.cpp
@@ -20,27 +20,30 @@ TEST_F(StateMachineTests, Run_ShouldStartWithInitialStateAndReturnContextAtTheEn
     // Arrange
     const auto initial_state_type = StateType::kRoot;
     const auto second_state_type = StateType::kShow;
+    const auto end_state_type = StateType::kEnd;
     auto initial_state = std::make_shared<MockState>();
     auto second_state = std::make_shared<MockState>();
 
     auto context = std::make_unique<StateContext>();
     auto states_factory = std::make_shared<MockStatesFactory>();
 
-    const int testing_times = 2;
+    const int testing_times = 5;
 
-    for (int i = 0; i < testing_times; ++i) {
-        EXPECT_CALL(*states_factory, GetState(initial_state_type)).WillOnce(Return(initial_state));
-        EXPECT_CALL(*states_factory, GetState(second_state_type)).WillOnce(Return(second_state));
-        EXPECT_CALL(*initial_state, Execute(::testing::_)).WillOnce(Return(second_state_type));
-        EXPECT_CALL(*second_state, Execute(::testing::_)).WillOnce(Return(StateType::kEnd));
-    }
     ConsoleStateMachine state_machine { initial_state_type, std::move(context), states_factory };
+
+    EXPECT_CALL(*states_factory, GetState(initial_state_type)).Times(testing_times).WillRepeatedly(Return(initial_state));
+    EXPECT_CALL(*initial_state, Execute(::testing::_)).Times(testing_times).WillRepeatedly(Return(second_state_type));
+    EXPECT_CALL(*states_factory, GetState(second_state_type)).Times(testing_times).WillRepeatedly(Return(second_state));
+    EXPECT_CALL(*second_state, Execute(::testing::_)).Times(testing_times).WillRepeatedly(Return(end_state_type));
+    EXPECT_CALL(*states_factory, GetState(end_state_type)).Times(testing_times).WillRepeatedly(Return(nullptr));
     // Act
-    std::vector<std::shared_ptr<StateContext>> actual_result_contexts;
+    std::vector<std::shared_ptr<StateContext>> actual_result_contexts(testing_times);
     for (int i = 0; i < testing_times; ++i)
-        actual_result_contexts.push_back(state_machine.Run());
+        actual_result_contexts[i] = state_machine.Run();
     // Assert
+    ASSERT_EQ(testing_times, actual_result_contexts.size());
     for (int i = 0; i < testing_times; ++i) {
+        ASSERT_NE(nullptr, actual_result_contexts[i]);
         for (int j = i + 1; j < testing_times; ++j) {
             EXPECT_NE(actual_result_contexts[i], actual_result_contexts[j]);
         }
