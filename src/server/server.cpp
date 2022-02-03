a05208994fb169ae5836d3bc6f80e27a1d084de0
Maksym Kharchenko <maksym.kharchenko@teamdev.com>
2022-02-03 15:28:46 +0200

    added options to server

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -9,19 +9,52 @@
 
 #include "logs/LogInit.h"
 #include "logs/DefaultLogging.h"
+#include <boost/program_options.hpp>
 
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 
-int main() {
-    logs_init();
+#include <iostream>
+
+std::string parse_options(int argc, char* argv[]) {
+    bool severity_debug = false;
+    std::string host = "0.0.0.0";
+    std::string port = "8586";
+
+    boost::program_options::options_description desc("Allowed options");
+
+    desc.add_options()
+            ("help", "prints help message")
+            ("debug", boost::program_options::bool_switch(&severity_debug), "enable debug logs")
+            ("host", boost::program_options::value(&host), "server host, default: 0.0.0.0")
+            ("port", boost::program_options::value(&port), "server port, default: 8586");
+
+    boost::program_options::variables_map vm;
+    boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+    boost::program_options::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return "";
+    }
+
+    if (severity_debug)
+        logs_init(boost::log::trivial::severity_level::debug);
+    else
+        logs_init(boost::log::trivial::severity_level::info);
+
+    return host + ":" + port;
+}
+
+int main(int argc, char* argv[]) {
+    std::string server_address = parse_options(argc, argv);
+    if (server_address.empty())
+        return 0;
 
     std::unique_ptr<Model> model =
             std::make_unique<TaskManager>(std::make_unique<IdGenerator>());
 
     GRPCServerEndPoint service { std::move(model) };
-
-    std::string server_address("0.0.0.0:8586");
 
     grpc::ServerBuilder builder;
     // Listen on the given address without any authentication mechanism.
@@ -31,11 +64,14 @@ int main() {
     builder.RegisterService(&service);
     // Finally assemble the server.
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-    //std::cout << "Server listening on " << server_address << std::endl;
 
-    //BOOST_LOG_NAMED_SCOPE(__FILE__);
-    //BOOST_LOG_NAMED_SCOPE(__FUNCTION__);
-    LOG_TRIVIAL(info) << "Server listening on " << server_address;
+    if (server) {
+        LOG_TRIVIAL(info) << "Server listening on " << server_address;
+    }
+    else {
+        LOG_TRIVIAL(error) << "Failed to start server on " << server_address;
+        return 1;
+    }
 
     // Wait for the server to shutdown. Note that some other thread must be
     // responsible for shutting down the server for this call to ever return.
